7533452b6a8b68a56dc591aebae8864e1aa487a6
pwfldatascientist <actokyo.815@gmail.com>
2025-10-21 16:18:08 +0900

    first commit

---
+++ example.py
@@ -0,0 +1,19 @@
+def greet():
+  print("こんにちは")
+
+def print_name(name):
+  print("私の名前は{}です".format(name))
+
+def get_greet():
+  grt = "おはようございます"
+  return grt
+
+def add(a,b):
+  return a+b
+
+
+print("hello world")
+greet()
+print_name(name="RareTECH-kun")
+print(get_greet())
+print(add(5,2))
